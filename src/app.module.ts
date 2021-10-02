cb5f9837ecaffe8a65f492c36c16009ccfd3c96b
Sander van Dinteren <sanvandinteren@gmail.com>
2021-10-02 15:54:00 +0200

    Added NestJS support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuizModule } from './quiz/quiz.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    QuizModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
